e82bce27090c2b7b5b3e77f29bb7487c190cc0a5
Nguyen Thanh Binh <thanh.binh.nguyen@wings-consulting.jp>
2021-06-15 15:34:43 +0900

    Add BruteCollinearPoints

---
+++ Week 3/Mergesort/Redo/BruteCollinearPoints.java
@@ -0,0 +1,21 @@
+package Mergesort.Redo;
+
+public class BruteCollinearPoints {
+    
+    private Point[] points;
+    private int numberOfPoints;
+    private int numberOfSegments;
+    private LineSegment[] segments;
+
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) throw new IllegalArgumentException();
+        this.points = points;
+        numberOfPoints = points.length;
+        
+    }
+
+    public int numberOfSegments() {}
+
+    public LineSegment[] segments() {}
+
+}
---
+++ Week 3/Mergesort/Redo/FastCollinearPoints.java
@@ -0,0 +1,5 @@
+package Mergesort.Redo;
+
+public class FastCollinearPoints {
+    
+}
